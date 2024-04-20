e5abad3cc606a52b60c101460e151816f44dc764
Felix01SA <felixsouza1200@gmail.com>
2024-04-20 12:31:17 -0300

    Server config command

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { dirname, importx } from '@discordx/importer';
 import { CustomItents as CustomIntents } from '@magicyan/discord';
 import { Client, DIService, tsyringeDependencyRegistryEngine } from 'discordx';
 import { container } from 'tsyringe';
+import { env } from '@lib/env';
 
 async function run() {
   DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container);
@@ -12,8 +13,7 @@ async function run() {
   });
 
   await importx(`${dirname(import.meta.url)}/core/**/*.ts`);
-
-  await client.login(process.env.BOT_TOKEN!);
+  await client.login(env.BOT_TOKEN);
 }
 
 void run();
